07d98deab13f1f0bd4672b70f3cc543157ab14cb
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2024-12-28 20:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+	return blob.size;
 };
+
+console.log(byteSize("hello world"));
+console.log(byteSize("안녕하세요"));
+console.log(byteSize(""));
 
 // Do not change the code below
 const str = prompt("Enter some string.");
